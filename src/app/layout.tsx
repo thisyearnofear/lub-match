cb02189cf9ec6d5808f37c3e78a253584cdf6fc2
Quincybob <74205675+Quincybob@users.noreply.github.com>
2025-02-14 05:23:13 +0800

    linting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Valentines: A Romantic Card Game with a Surprise Proposal",
-  description:
-    "Play a unique Valentine's card game. Complete the collection to reveal a romantic proposal!",
+  title: "Muthoni's Valentine",
+  description: "Pumzika Couple",
   keywords: [
     "Valentine's card game",
     "romantic proposal game",
